ee6949257258cf224e61a932868281f957d2c7cf
thankGodForLife <28709874+xodiumluma@users.noreply.github.com>
2018-06-25 22:33:29 +1000

    Set up failing test
    We now use the chai-as-promised
    library
    This library is meant for objects and
    arrays only, not for primitives like
    booleans

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -2,6 +2,11 @@ import runMe from './index';
 import { expect } from 'chai';
 import 'mocha';
 
+import chai = require("chai");
+import chaiAsPromised = require("chai-as-promised");
+chai.use(chaiAsPromised);
+const assert: any = chai.assert;
+
 
 describe('Main function', () => {
 
@@ -9,6 +14,11 @@ describe('Main function', () => {
 	it('should return a Promise', () => {
 		expect(runMe({})).to.be.instanceof(Promise);
 	});
+
+	it('should return a rejected Promise given a boolean input', (): void => {
+		return assert.isRejected(runMe(false), false);
+	});
+
 
 
 
