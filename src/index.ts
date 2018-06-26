d72c77c230f9dd57c7f42ee2733ea8982c27c2f1
thankGodForLife <28709874+xodiumluma@users.noreply.github.com>
2018-06-26 23:21:19 +1000

    Add static type parameters

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 const process = <T>(target: T): Promise<T>  => {
-	return new Promise<T>((resolve: any, reject: any) => {
+		return new Promise<T>((resolve: (resolvedValue: any | void) => void, reject: (rejectedValue: any | void) => void) => {
 		if (typeof target === "boolean") {
 			reject("Cannot be boolean");
 		}
