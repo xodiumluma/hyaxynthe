873a99724d37e9d79d0e1512e5463f87ae2dcd8c
thankGodForLife <28709874+xodiumluma@users.noreply.github.com>
2018-06-29 11:16:44 +1000

    Pass test for undefined input

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,10 @@ const process = <T>(target: T): Promise<T>  => {
 		return new Promise<T>((resolve: (resolvedValue: any | void) => void, reject: (rejectedValue: any | void) => void) => {
 			const whatType = typeof target;
 		if (
-			whatType === "boolean" || 
-			whatType === "string" ||
-			whatType === "number"
+			whatType === "boolean"   || 
+			whatType === "string"    ||
+			whatType === "number"    ||
+			whatType === "undefined"
 		) {
 			reject("Cannot be " + whatType);
 		}
