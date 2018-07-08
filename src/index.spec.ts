acc29f8c2589a97a7218eda9444a2c563fcd4c58
thankGodForLife <28709874+xodiumluma@users.noreply.github.com>
2018-07-09 00:16:59 +1000

    Generify hyaxynthe calls whenever possible

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -24,7 +24,7 @@ describe('Main function', (): void => {
 
 
 	it('should return a Promise', (): void => {
-		expect(hyaxynthe({})).to.be.instanceof(Promise);
+		expect(hyaxynthe<object>({})).to.be.instanceof(Promise);
 	});
 
 
@@ -35,38 +35,38 @@ describe('Main function', (): void => {
 	// invalid types
 
 	it('should return a rejected Promise given a boolean input', async (): Promise<void> => {
-		return assert.isRejected(hyaxynthe(false), false);
+		return assert.isRejected(hyaxynthe<boolean>(false), false);
 	});
 
 	it('should return a rejected Promise given a string input', async (): Promise<void> => {
-		return assert.isRejected(hyaxynthe("string"), false);
+		return assert.isRejected(hyaxynthe<string>("string"), false);
 	});
 
 
 	it('should return a rejected Promise given a numeric input', async (): Promise<void> => {
-		return assert.isRejected(hyaxynthe(4), false);
+		return assert.isRejected(hyaxynthe<number>(4), false);
 	});
 
 	it('should return a rejected Promise given a float input', async (): Promise<void> => {
-		return assert.isRejected(hyaxynthe(4.0), false);
+		return assert.isRejected(hyaxynthe<number>(4.0), false);
 	});
 
 	it('should return a rejected Promise given a NaN input', async (): Promise<void> => {
-		return assert.isRejected(hyaxynthe(+"a"), false);
+		return assert.isRejected(hyaxynthe<number>(+"a"), false);
 	});
 
 	it('should return a rejected Promise given a function', async (): Promise<void> => {
-		return assert.isRejected(hyaxynthe(function () {}), false);
+		return assert.isRejected(hyaxynthe<Function>(function () {}), false);
 	});
 
 
 	it('should return a rejected Promise given an undefined input', async (): Promise<void> => {
-		return assert.isRejected(hyaxynthe(void 0), false);
+		return assert.isRejected(hyaxynthe<any>(void 0), false);
 	});
 
 
 	it('should return a rejected Promise given a null input', async (): Promise<void> => {
-		return assert.isRejected(hyaxynthe(null), false);
+		return assert.isRejected(hyaxynthe<any>(null), false);
 	});
 
 
@@ -77,7 +77,7 @@ describe('Main function', (): void => {
 	});
 
 	it('should return a rejected Promise given two empty objects (multiple valid inputs)', async (): Promise<void> => {
-		return assert.isRejected(hyaxynthe({}, {}), false);
+		return assert.isRejected(hyaxynthe<object>({}, {}), false);
 	});
 
 });
