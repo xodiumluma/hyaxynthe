9581d1fadb3b7cbb5ff074258f3f69691dae0153
thankGodForLife <28709874+xodiumluma@users.noreply.github.com>
2018-10-13 22:56:37 +1100

    Modularise function
    Break it down

---
+++ src/runtimecheck.ts
@@ -0,0 +1,15 @@
+export const runtimeCheck = <T>(target: T): Promise<T>  => {
+		return new Promise<T>((resolve: (resolvedValue: any | void) => void, reject: (rejectedValue: any | void) => void) => {
+			const whatType = typeof target;
+		if (
+			whatType === "boolean"   || 
+			whatType === "string"    ||
+			whatType === "number"    ||
+			whatType === "function"  ||
+			whatType === "undefined" ||
+			(whatType === "object" && target + "" === "null")
+		) {
+			reject("Cannot be " + whatType);
+		}
+	});
+};
