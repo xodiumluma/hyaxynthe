bb8296e1cf9411cbb68a50e9b092347a753030ed
AgileTec Solutions <28709874+xodiumluma@users.noreply.github.com>
2022-01-30 21:44:19 +1100

    'Reformat' parameter check
    a) use arrow function as there is no 'this'
    required
    b) use ReadonlyArray - sure its 'arguments' 
    but always favour immutable over mutable
    unless with good cause
    c) don't mutate the reference, just return
    the first member

--- src/index.ts
+++ src/index.ts
@@ -2,12 +2,12 @@ import { runtimeCheck } from './runtimeCheck';
 
 
 
-export function parameterCheck<T> (...target: T[]): Promise<T> {
+export const parameterCheck = <T> (...target: ReadonlyArray<T>): Promise<T> => {
 	if (!arguments.length) { 
 		return Promise.reject("hyaxynthe does not support no input. Please provide an object or array input"); 
 	} 
 	if (arguments.length > 1) { 
 		return Promise.reject("hyaxynthe only supports one input"); 
 	}
-	return runtimeCheck<T>(target.pop());
+	return runtimeCheck<T>(target[0]);
 }
