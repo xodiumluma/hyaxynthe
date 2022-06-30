b27bd518167939326a7bc12fa6f3c682fd42db02
AgileTec Solutions <28709874+xodiumluma@users.noreply.github.com>
2022-07-01 08:56:16 +1000

    [chore] indentation

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,11 @@
 import { runtimeCheck } from './runtimeCheck';
 
-
-
 export const parameterCheck = <T> (...target: ReadonlyArray<T>): Promise<T> => {
-	if (!arguments.length) { 
-		return Promise.reject("hyaxynthe does not support no input. Please provide an object or array input"); 
-	} 
-	if (arguments.length > 1) { 
-		return Promise.reject("hyaxynthe only supports one input"); 
-	}
-	return runtimeCheck<T>(target[0]);
+  if (!arguments.length) { 
+    return Promise.reject("hyaxynthe does not support no input. Please provide an object or array input"); 
+  } 
+  if (arguments.length > 1) { 
+    return Promise.reject("hyaxynthe only supports one input"); 
+  }
+  return runtimeCheck<T>(target[0]);
 }
