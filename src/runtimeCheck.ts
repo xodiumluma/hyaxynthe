abb487fae7de6f95d0f354dc41091cda49f602af
thankGodForLife <28709874+xodiumluma@users.noreply.github.com>
2022-08-18 11:17:27 +1000

    Modernise

--- src/runtimeCheck.ts
+++ src/runtimeCheck.ts
@@ -1,13 +1,13 @@
 export const runtimeCheck = <T>(target: T): Promise<T>  => {
 	return new Promise<T>((resolve: (resolvedValue: any | void) => void, reject: (rejectedValue: string) => void) => {
-			const whatType: string = typeof target;
+		const whatType: string = typeof target;
 		if (
 			whatType === "boolean"   || 
 			whatType === "string"    ||
 			whatType === "number"    ||
 			whatType === "function"  ||
 			whatType === "undefined" ||
-			(whatType === "object" && target + "" === "null")
+			(whatType === "object" && `${target}` === "null")
 		) {
 			reject("Cannot be " + whatType);
 		}
